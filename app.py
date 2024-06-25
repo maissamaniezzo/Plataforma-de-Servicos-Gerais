399fc68885851ac7e3149f12f82d1b74fe5c91a7
Maíssa Maniezzo <maissamaniezzo@gmail.com>
2024-06-25 18:47:11 -0300

    Adicionar Flask
    Alterar arvore de diretorios e adicionar funções nas paginas para conectar <head>

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
